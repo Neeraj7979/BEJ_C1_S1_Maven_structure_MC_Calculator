6ad023f38c9700a2cf50224f5883e500ef66fa07
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-20 17:39:26 +0530

    Created test cases for modular division  for modular division method in calculator class

--- src/test/java/com/niit/bej/CalculatorTest.java
+++ src/test/java/com/niit/bej/CalculatorTest.java
@@ -12,25 +12,26 @@ class CalculatorTest {
 
     @BeforeEach
     void setUp() {
-        calculator=new Calculator();
+        calculator = new Calculator();
     }
 
     @AfterEach
     void tearDown() {
-        calculator=null;
+        calculator = null;
     }
 
     @Test
     void addSuccess() {
-        double expected=15;
-        double actual=calculator.add(5,10);
-        assertEquals(expected,actual);
+        double expected = 15;
+        double actual = calculator.add(5, 10);
+        assertEquals(expected, actual);
 
     }
+
     @Test
     void addFailure() {
-        double expected=12;
-        double actual=calculator.add(5,10);
+        double expected = 12;
+        double actual = calculator.add(5, 10);
         assertNotEquals(expected, actual);
 
     }
@@ -51,14 +52,30 @@ class CalculatorTest {
     }
 
     @Test
-    void multiply() {
+    void multiplySuccess() {
+        double expected = 50;
+        double actual = calculator.multiply(5, 10);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void divide() {
+    void multiplyFailure() {
+        double expected = 45;
+        double actual = calculator.multiply(5, 10);
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    void divideByZero() {
+        double expected = 0;
+        double actual = calculator.divide(0, 34);
+        assertEquals(expected, actual);
     }
 
     @Test
     void modularDivision() {
+        double expected = 0;
+        double actual = calculator.modularDivision(10, 5);
+        assertEquals(expected, actual);
     }
 }
